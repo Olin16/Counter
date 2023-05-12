569a27b3c228d35924663720e78c811cff84600f
Owenlin <owenlin1604@gmail.com>
2023-05-12 13:03:33 -0400

    Initial commit

---
+++ src/Queue.java
@@ -0,0 +1,12 @@
+public class Counter implements DataStructure{
+
+    @Override
+    public boolean put(Object obj) {
+        return false;
+    }
+
+    @Override
+    public Object get() {
+        return null;
+    }
+}
